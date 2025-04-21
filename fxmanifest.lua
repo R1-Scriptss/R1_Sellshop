74e6452bd620b2c440457b1f57cab9dff42dd658
R1 Scripts <fonkysekytb@gmail.com>
2025-04-21 18:24:36 +0200

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,20 +1,20 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'R1-Scripts'
-description 'Crafting system for ESX and QB'
+author 'R1'
+description 'Premuim Sell Shop'
 version '1.0.0'
+
 lua54 'yes'
 
-client_scripts {
-    'client/*.lua',
-}
+shared_script '@ox_lib/init.lua'
+shared_script 'config.lua'
 
-server_scripts {
-    'server/*.lua',
-}
+client_script 'client.lua'
+server_script 'server.lua'
 
-shared_scripts {
-	'@ox_lib/init.lua',
-	'config.lua',
+dependencies {
+    'ox_inventory',
+    'ox_target',
+    'ox_lib'
 }
